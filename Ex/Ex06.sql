25082ea9eaf6567c86c2460e5790d1d3dd344c5a
경태윤 <xodbsrud320@gmail.com>
2021-12-21 11:50:46 +0900

    join

--- Ex/Ex06.sql
+++ Ex/Ex06.sql
@@ -1,34 +1,100 @@
--- 단일행 서브쿼리
-select  first_name,
-        salary
-from employees
-where salary >= "Den의 급여";
-
-select  salary
-from employees
-where first_name = 'Den';
-
--->
+-- 복습
 select  first_name,
         salary,
-        phone_number,
         department_id
-from employees
-where salary >= (select salary
-                 from employees
-                 where first_name = 'Den');
+from employees;
 
--- 급여를 가장 적게 받는 사람의 이름, 급여, 사원번호 출력                 
 select  first_name,
         salary,
-        employee_id
-from employees
-where salary = (select min(salary)
-                from employees);
-                
--- 평균 급여보다 적게 받는 사람의 이름, 급여 출력
-select  first_name,
-        salary
-from employees
-where salary < (select avg(salary)
-                from employees);
+        de.department_id,
+        department_name
+from employees em, departments de
+where em.department_id = de.department_id;
+
+select  count(*)
+from employees em, departments de
+where em.department_id = de.department_id;
+
+select  em.first_name,
+        de.department_id,
+        de.department_name,
+        jo.job_id,
+        jo.job_title
+from employees em, departments de, jobs jo
+where em.department_id = de.department_id
+and em.job_id = jo.job_id;
+
+-- outer join > *left* outer join, 표준 쿼리
+select  em.first_name,
+        em.salary,
+        em.department_id,
+        de.department_id,
+        de.department_name
+from employees em left outer join departments de
+on em.department_id = de.department_id;
+
+-- 오라클에서만 쓰는 표현
+select  em.first_name,
+        em.salary,
+        de.department_id,
+        de.department_name
+from employees em, departments de
+where em.department_id = de.department_id(+);
+
+-- *right* outer join
+select  em.first_name,
+        em.salary,
+        de.department_id,
+        de.department_name
+from employees em right outer join departments de
+on em.department_id = de.department_id;
+
+select  em.first_name,
+        em.salary,
+        de.department_id,
+        de.department_name
+from employees em, departments de
+where em.department_id(+) = de.department_id;
+
+-- right outer join -> left outer join
+select  em.first_name,
+        em.salary,
+        em.department_id,
+        de.department_id,
+        de.department_name
+from departments de left outer join employees em
+on em.department_id = de.department_id;
+
+select  em.first_name,
+        em.salary,
+        de.department_id,
+        de.department_name
+from departments de, employees em
+where em.department_id = de.department_id(+);
+
+-- full outer join: (+)사용은 불가
+select  em.first_name,
+        de.department_id,
+        de.department_name
+from employees em full outer join departments de
+on em.department_id = de.department_id;
+
+-- self join
+select  employee_id,
+        first_name,
+        phone_number,
+        salary,
+        manager_id
+from employees;
+
+select  em.employee_id,
+        em.first_name,
+        em.salary,
+        em.phone_number,
+        em.manager_id,
+        ma.manager_id,
+        ma.first_name as manager,
+        ma.phone_number,
+        ma.email
+from employees em, employees ma
+where em.manager_id = ma.employee_id;
